0334bfbc40d7b1a3cde68956e827cac6946912fc
mic_x01 <m.marques.dev@gmail.com>
2023-11-10 00:52:02 -0300

    chore: go - using a replacer with a writer

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -20,9 +20,15 @@ func writeFormatted(writer io.Writer, template string, vals ...interface{}) {
 	fmt.Fprintf(writer, template, vals...)
 }
 
+func writeReplaced(writer io.Writer, str string, subs ...string) {
+	replacer := strings.NewReplacer(subs...)
+	replacer.WriteString(writer, str)
+}
+
 func main() {
+	text := "It was a boat. A small boat."
+	subs := []string{"boat", "kayak", "small", "huge"}
 	var writer strings.Builder
-	template := "Name: %s, Category: %s, Price: $%.2f"
-	writeFormatted(&writer, template, "Kayak", "Watersports", float64(279))
+	writeReplaced(&writer, text, subs...)
 	fmt.Println(writer.String())
 }
