632e424c9c78f56dbef501e9da2a645bbe54f5c3
mic_x01 <m.marques.dev@gmail.com>
2023-11-10 00:12:11 -0300

    chore: go - performing buffered writes

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"bufio"
-	"io"
+	// "io"
 	"strings"
 )
 
 func main() {
 	text := "It was a boat. A small boat."
-	var reader io.Reader = NewCustomReader(strings.NewReader(text))
-	var writer strings.Builder
-	slice := make([]byte, 5)
-	buffered := bufio.NewReader(reader)
-	for {
-		count, err := buffered.Read(slice)
-		if count > 0 {
-			Printfln("Buffer size: %v, buffered: %v",
-				buffered.Size(), buffered.Buffered())
-			writer.Write(slice[0:count])
-		}
-		if err != nil {
+	var builder strings.Builder
+	var writer = bufio.NewWriterSize(NewCustomWriter(&builder), 20)
+	for i := 0; true; {
+		end := i + 5
+		if end >= len(text) {
+			writer.Write([]byte(text[i:]))
+			writer.Flush()
 			break
 		}
+		writer.Write([]byte(text[i:end]))
+		i = end
 	}
-	Printfln("Read data: %v", writer.String())
+	Printfln("Written data: %v", builder.String())
 }
