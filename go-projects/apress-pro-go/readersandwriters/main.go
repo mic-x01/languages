88527e2a91043332972ea9ca112b64f433a6ba15
mic_x01 <m.marques.dev@gmail.com>
2023-11-10 01:03:48 -0300

    chore: go - encoding json data

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	// "bufio"
 	"fmt"
-	"io"
+	// "io"
+	"encoding/json"
 	"strings"
 )
 
@@ -16,19 +17,25 @@ import (
 // 	return fmt.Fscan(reader, val)
 // }
 
-func writeFormatted(writer io.Writer, template string, vals ...interface{}) {
-	fmt.Fprintf(writer, template, vals...)
-}
-
-func writeReplaced(writer io.Writer, str string, subs ...string) {
-	replacer := strings.NewReplacer(subs...)
-	replacer.WriteString(writer, str)
-}
+// func writeFormatted(writer io.Writer, template string, vals ...interface{}) {
+// 	fmt.Fprintf(writer, template, vals...)
+// }
+//
+// func writeReplaced(writer io.Writer, str string, subs ...string) {
+// 	replacer := strings.NewReplacer(subs...)
+// 	replacer.WriteString(writer, str)
+// }
 
 func main() {
-	text := "It was a boat. A small boat."
-	subs := []string{"boat", "kayak", "small", "huge"}
+	var b bool = true
+	var str string = "Hello"
+	var fval float64 = 99.99
+	var ival int = 200
+	var pointer *int = &ival
 	var writer strings.Builder
-	writeReplaced(&writer, text, subs...)
-	fmt.Println(writer.String())
+	encoder := json.NewEncoder(&writer)
+	for _, val := range []interface{}{b, str, fval, ival, pointer} {
+		encoder.Encode(val)
+	}
+	fmt.Print(writer.String())
 }
