4434b99f5ca6954c7def70e47e639b00411d884e
mic_x01 <m.marques.dev@gmail.com>
2023-11-10 11:59:41 -0300

    chore: go - encoding structs

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -7,12 +7,8 @@ import (
 )
 
 func main() {
-	m := map[string]float64{
-		"Kayak":      279,
-		"Lifejacket": 49.95,
-	}
 	var writer strings.Builder
 	encoder := json.NewEncoder(&writer)
-	encoder.Encode(m)
+	encoder.Encode(Kayak)
 	fmt.Print(writer.String())
 }
