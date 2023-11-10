6a1f57dad986cd2061d8a0a310536a370023b30f
mic_x01 <m.marques.dev@gmail.com>
2023-11-10 11:43:53 -0300

    chore: go - encoding maps

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	// "bufio"
-	"fmt"
-	// "io"
 	"encoding/json"
+	"fmt"
 	"strings"
 )
 
-// func scanFromReader(reader io.Reader, template string,
-// 	vals ...interface{}) (int, error) {
-// 	return fmt.Fscanf(reader, template, vals...)
-// }
-//
-// func scanSingle(reader io.Reader, val interface{}) (int, error) {
-// 	return fmt.Fscan(reader, val)
-// }
-
-// func writeFormatted(writer io.Writer, template string, vals ...interface{}) {
-// 	fmt.Fprintf(writer, template, vals...)
-// }
-//
-// func writeReplaced(writer io.Writer, str string, subs ...string) {
-// 	replacer := strings.NewReplacer(subs...)
-// 	replacer.WriteString(writer, str)
-// }
-
 func main() {
-	names := []string{"Kayak", "Lifejacket", "Soccer Ball"}
-	numbers := [3]int{10, 20, 30}
-	var byteArray [5]byte
-	copy(byteArray[0:], []byte(names[0]))
-	byteSlice := []byte(names[0])
+	m := map[string]float64{
+		"Kayak":      279,
+		"Lifejacket": 49.95,
+	}
 	var writer strings.Builder
 	encoder := json.NewEncoder(&writer)
-	encoder.Encode(names)
-	encoder.Encode(numbers)
-	encoder.Encode(byteArray)
-	encoder.Encode(byteSlice)
+	encoder.Encode(m)
 	fmt.Print(writer.String())
 }
