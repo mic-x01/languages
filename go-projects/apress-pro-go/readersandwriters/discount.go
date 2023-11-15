64958e85641ba3c1ef20bb3d628658d93164ebb8
mic_x01 <m.marques.dev@gmail.com>
2023-11-14 23:31:46 -0300

    chore: go - creating completely custom json decoders

--- go-projects/apress-pro-go/readersandwriters/discount.go
+++ go-projects/apress-pro-go/readersandwriters/discount.go
@@ -1,6 +1,9 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strconv"
+)
 
 type DiscountedProduct struct {
 	*Product `json:",omitempty"`
@@ -14,6 +17,32 @@ func (dp *DiscountedProduct) MarshalJSON() (jsn []byte, err error) {
 			"cost":    dp.Price - dp.Discount,
 		}
 		jsn, err = json.Marshal(m)
+	}
+	return
+}
+
+func (dp *DiscountedProduct) UnmarshalJSON(data []byte) (err error) {
+	mdata := map[string]interface{}{}
+	err = json.Unmarshal(data, &mdata)
+	if dp.Product == nil {
+		dp.Product = &Product{}
+	}
+	if err == nil {
+		if name, ok := mdata["Name"].(string); ok {
+			dp.Name = name
+		}
+		if category, ok := mdata["Category"].(string); ok {
+			dp.Category = category
+		}
+		if price, ok := mdata["Price"].(float64); ok {
+			dp.Price = price
+		}
+		if discount, ok := mdata["Offer"].(string); ok {
+			fpval, fperr := strconv.ParseFloat(discount, 64)
+			if fperr == nil {
+				dp.Discount = fpval
+			}
+		}
 	}
 	return
 }
