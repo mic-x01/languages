8eb179f76a5d6cd9e93eeab1a6bd27a107afb62b
mic_x01 <m.marques.dev@gmail.com>
2023-11-14 22:41:15 -0300

    chore: go - decoding number values

--- go-projects/apress-pro-go/readersandwriters/main.go
+++ go-projects/apress-pro-go/readersandwriters/main.go
@@ -11,6 +11,7 @@ func main() {
 	reader := strings.NewReader(`true "Hello" 9.99 200`)
 	vals := []interface{}{}
 	decoder := json.NewDecoder(reader)
+	decoder.UseNumber()
 	for {
 		var decodedVal interface{}
 		err := decoder.Decode(&decodedVal)
@@ -23,6 +24,16 @@ func main() {
 		vals = append(vals, decodedVal)
 	}
 	for _, val := range vals {
-		Printfln("Decoded (%T): %v", val, val)
+		if num, ok := val.(json.Number); ok {
+			if ival, err := num.Int64(); err == nil {
+				Printfln("Decoded Integer: %v", ival)
+			} else if fpval, err := num.Float64(); err == nil {
+				Printfln("Decoded Floating Point: %v", fpval)
+			} else {
+				Printfln("Decoded String: %v", num.String())
+			}
+		} else {
+			Printfln("Decoded (%T): %v", val, val)
+		}
 	}
 }
