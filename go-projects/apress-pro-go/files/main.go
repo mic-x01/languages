770a85d80c5a804c093539706c9fe65c15bb9a83
mic_x01 <m.marques.dev@gmail.com>
2023-11-20 16:04:20 -0300

    chore: go - working with file paths

--- go-projects/apress-pro-go/files/main.go
+++ go-projects/apress-pro-go/files/main.go
@@ -3,23 +3,20 @@ package main
 import (
 	// "fmt"
 	"os"
+	"path/filepath"
 	// "time"
-	"encoding/json"
+	// "encoding/json"
 )
 
 func main() {
-	cheapProducts := []Product{}
-	for _, p := range Products {
-		if p.Price < 100 {
-			cheapProducts = append(cheapProducts, p)
-		}
-	}
-	file, err := os.CreateTemp(".", "temp-*.json")
+	path, err := os.UserHomeDir()
 	if err == nil {
-		defer file.Close()
-		encoder := json.NewEncoder(file)
-		encoder.Encode(cheapProducts)
-	} else {
-		Printfln("Error: %v", err.Error())
+		path = filepath.Join(path, "MyApp", "MyTempFile.json")
 	}
+	Printfln("Full path: %v", path)
+	Printfln("Volume name: %v", filepath.VolumeName(path))
+	Printfln("Dir component: %v", filepath.Dir(path))
+	Printfln("File component: %v", filepath.Base(path))
+	Printfln("File extension: %v", filepath.Ext(path))
+
 }
