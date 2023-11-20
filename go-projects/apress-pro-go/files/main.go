77b3a8eb0a06d213eb74ac0a841bc6bcb7812ce7
mic_x01 <m.marques.dev@gmail.com>
2023-11-20 16:19:58 -0300

    chore: go - managing files and directories

--- go-projects/apress-pro-go/files/main.go
+++ go-projects/apress-pro-go/files/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	// "time"
-	// "encoding/json"
+	"encoding/json"
 )
 
 func main() {
@@ -14,9 +14,16 @@ func main() {
 		path = filepath.Join(path, "MyApp", "MyTempFile.json")
 	}
 	Printfln("Full path: %v", path)
-	Printfln("Volume name: %v", filepath.VolumeName(path))
-	Printfln("Dir component: %v", filepath.Dir(path))
-	Printfln("File component: %v", filepath.Base(path))
-	Printfln("File extension: %v", filepath.Ext(path))
-
+	err = os.MkdirAll(filepath.Dir(path), 0766)
+	if err == nil {
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0666)
+		if err == nil {
+			defer file.Close()
+			encoder := json.NewEncoder(file)
+			encoder.Encode(Products)
+		}
+	}
+	if err != nil {
+		Printfln("Error %v", err.Error())
+	}
 }
