684ed9755916be3638aae5c4f7e8589dd91c4fa2
mic_x01 <m.marques.dev@gmail.com>
2023-11-20 23:43:48 -0300

    chore: go - processing all files in a directory

--- go-projects/apress-pro-go/files/main.go
+++ go-projects/apress-pro-go/files/main.go
@@ -9,15 +9,16 @@ import (
 	// "encoding/json"
 )
 
+func callback(path string, dir os.DirEntry, dirErr error) (err error) {
+	info, _ := dir.Info()
+	Printfln("Path %v, Size: %v", path, info.Size())
+	return
+}
+
 func main() {
 	path, err := os.Getwd()
 	if err == nil {
-		matches, err := filepath.Glob(filepath.Join(path, "*.json"))
-		if err == nil {
-			for _, m := range matches {
-				Printfln("Match: %v", m)
-			}
-		}
+		err = filepath.WalkDir(path, callback)
 	}
 	if err != nil {
 		Printfln("Error %v", err.Error())
