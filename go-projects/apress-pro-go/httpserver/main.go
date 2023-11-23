88474357c802efc61403a55980db576273557f97
mic_x01 <m.marques.dev@gmail.com>
2023-11-22 22:15:23 -0300

    chore: go - creating a simple http server

---
+++ go-projects/apress-pro-go/httpserver/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type StringHandler struct {
+	message string
+}
+
+func (sh StringHandler) ServeHTTP(writer http.ResponseWriter,
+	request *http.Request) {
+	io.WriteString(writer, sh.message)
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", StringHandler{message: "Hello, World"})
+	if err != nil {
+		Printfln("Error: %v", err.Error())
+	}
+}
