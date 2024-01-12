a02b4fa1cd1ead787e6351614a3c884823bb414f
mic_x01 <m.marques.dev@gmail.com>
2024-01-12 03:51:05 -0300

    chore: go - sending simple HTTP requests

--- go-projects/apress-pro-go/httpclient/main.go
+++ go-projects/apress-pro-go/httpclient/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	// "io"
 	"net/http"
+	// "os"
 	"time"
 )
 
@@ -11,6 +14,21 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 
 	time.Sleep(time.Second)
+	response, err := http.Get("http://localhost:5000/json")
+	if err == nil && response.StatusCode == http.StatusOK {
+		defer response.Body.Close()
+		data := []Product{}
+		err = json.NewDecoder(response.Body).Decode(&data)
+		if err == nil {
+			for _, p := range data {
+				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
+			}
+		} else {
+			Printfln("Decode error: %v", err.Error())
+		}
+	} else {
+		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+	}
 
 	Printfln("Press any key...")
 	fmt.Scanln()
