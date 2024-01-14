3ffb3aeed45563b6d7cade6eed7393073f9891a5
mic_x01 <m.marques.dev@gmail.com>
2024-01-14 03:07:22 -0300

    chore: go - sending POST Requests

--- go-projects/apress-pro-go/httpclient/main.go
+++ go-projects/apress-pro-go/httpclient/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	// "io"
+	"io"
 	"net/http"
-	// "os"
+	"os"
 	"time"
 )
 
@@ -14,18 +14,15 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 
 	time.Sleep(time.Second)
-	response, err := http.Get("http://localhost:5000/json")
+	formData := map[string][]string{
+		"name":     {"Kayak"},
+		"category": {"Watersports"},
+		"price":    {"279"},
+	}
+	response, err := http.PostForm("http://localhost:5000/echo", formData)
 	if err == nil && response.StatusCode == http.StatusOK {
+		io.Copy(os.Stdout, response.Body)
 		defer response.Body.Close()
-		data := []Product{}
-		err = json.NewDecoder(response.Body).Decode(&data)
-		if err == nil {
-			for _, p := range data {
-				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
-			}
-		} else {
-			Printfln("Decode error: %v", err.Error())
-		}
 	} else {
 		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
 	}
