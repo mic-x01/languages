7e51d1201d2d3023195f7a49d30d23152caafb5c
mic_x01 <m.marques.dev@gmail.com>
2024-01-14 03:15:37 -0300

    chore: go - Posting a Form Using a Reader

--- go-projects/apress-pro-go/httpclient/main.go
+++ go-projects/apress-pro-go/httpclient/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -14,17 +15,20 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 
 	time.Sleep(time.Second)
-	formData := map[string][]string{
-		"name":     {"Kayak"},
-		"category": {"Watersports"},
-		"price":    {"279"},
-	}
-	response, err := http.PostForm("http://localhost:5000/echo", formData)
-	if err == nil && response.StatusCode == http.StatusOK {
-		io.Copy(os.Stdout, response.Body)
-		defer response.Body.Close()
+	var builder strings.Builder
+	err := json.NewEncoder(&builder).Encode(Products[0])
+	if err == nil {
+		response, err := http.Post("http://localhost:5000/echo",
+			"application/json",
+			strings.NewReader(builder.String()))
+		if err == nil && response.StatusCode == http.StatusOK {
+			io.Copy(os.Stdout, response.Body)
+			defer response.Body.Close()
+		} else {
+			Printfln("Error: %v", err.Error())
+		}
 	} else {
-		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+		Printfln("Error: %v", err.Error())
 	}
 
 	Printfln("Press any key...")
