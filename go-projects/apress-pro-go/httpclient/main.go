9641180b9d02ec35e4f946a819a0b4b88e102d10
mic_x01 <m.marques.dev@gmail.com>
2024-01-14 03:35:14 -0300

    chore: go - Using the Convenience Functions to Create a Request

--- go-projects/apress-pro-go/httpclient/main.go
+++ go-projects/apress-pro-go/httpclient/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
+	// "net/url"
 	"os"
 	"strings"
 	"time"
@@ -19,25 +19,19 @@ func main() {
 	var builder strings.Builder
 	err := json.NewEncoder(&builder).Encode(Products[0])
 	if err == nil {
-		reqURL, err := url.Parse("http://localhost:5000/echo")
+		req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/echo",
+			io.NopCloser(strings.NewReader(builder.String())))
 		if err == nil {
-			req := http.Request{
-				Method: http.MethodPost,
-				URL:    reqURL,
-				Header: map[string][]string{
-					"Content-Type": {"application/json"},
-				},
-				Body: io.NopCloser(strings.NewReader(builder.String())),
-			}
-			response, err := http.DefaultClient.Do(&req)
+			req.Header["Content-Type"] = []string{"application/json"}
+			response, err := http.DefaultClient.Do(req)
 			if err == nil && response.StatusCode == http.StatusOK {
 				io.Copy(os.Stdout, response.Body)
 				defer response.Body.Close()
 			} else {
-				Printfln("Error: %v", err.Error())
+				Printfln("Request Error: %v", err.Error())
 			}
 		} else {
-			Printfln("Parse Error: %v", err.Error())
+			Printfln("request Init Error: %v", err.Error())
 		}
 	} else {
 		Printfln("Error: %v", err.Error())
